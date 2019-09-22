75185f82df1fcbf3d974ce1d050440d3fd188c98
aitrek <aitrek.zh@gmail.com>
2019-09-22 19:57:06 +0800

    add ShortSideTransform for ctpn

--- deepsight/utils/transforms.py
+++ deepsight/utils/transforms.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 from utils.image import resize
 
@@ -42,3 +43,27 @@ class UnifySize:
             target[0:height, 0:width] = img
 
         return target
+
+
+class ShortSideTransform:
+    """
+    Resize image by setting its short side to 600 with original aspect ratio.
+
+    Args:
+        short_side (int): The target image's short side.
+    """
+    def __init__(self, short_side: int):
+        self._short_side = short_side
+
+    def __call__(self, img: np.array):
+        height, width, _ = img.shape
+        scale = self._short_side / min(height, width)
+        resize_height = int(height * scale)
+        resize_width = int(width * scale)
+        resize_img = cv2.resize(img, (resize_height, resize_width),
+                                cv2.INTER_NEAREST)
+        return resize_img
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(short_side={0})'.\
+            format(self._short_side)
