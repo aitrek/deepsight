cac4d6d7afd87f5aa81c94beafff0f53b9075237
aitrek <aitrek.zh@gmail.com>
2019-11-07 13:53:49 +0800

    fix ctpn predict error

--- deepsight/networks/ctpn/predict.py
+++ deepsight/networks/ctpn/predict.py
@@ -5,6 +5,7 @@ from ...utils.nms import nms
 
 TEXT_NON_TEXT_THRESHOLD = 0.7
 NMS_THRESH = 0.3
+GROUP_MIN_SAMPLES = 3
 
 
 class CTPNPredict(Predict):
@@ -17,7 +18,7 @@ class CTPNPredict(Predict):
             return []
 
         nms_boxes = [box[:4] for box in boxes]
-        nms_score = [box[4].item() for box in boxes]
+        nms_score = [box[4] for box in boxes]
         idxes = nms(nms_boxes, nms_score, NMS_THRESH)
         boxes = [boxes[idx] for idx in idxes]
         groups = self._group_boxes(boxes)
@@ -34,16 +35,16 @@ class CTPNPredict(Predict):
         for y in range(height):
             for x in range(width):
                 for z in range(k):
-                    score = scores[0, 2 * z + 1, y, x]
+                    score = float(scores[0, 2 * z + 1, y, x])
                     if score > threshold:
                         vc = vcoords[0, 2 * z, y, x]
                         vh = vcoords[0, 2 * z + 1, y, x]
-                        offset = sides[0, z, y, x] * fixed_width
+                        offset = float(sides[0, z, y, x] * fixed_width)
                         ha = ANCHOR_HEIGHTS[z]
 
                         cy = vc * ha + y * fixed_width
                         h = math.exp(vh) * ha
-                        left = math.floor(x * fixed_width - (fixed_width - 1) / 2)
+                        left = math.floor(x * fixed_width + (fixed_width - 1) / 2)
                         right = math.ceil(x * fixed_width + (fixed_width - 1) / 2)
                         top = math.floor(cy - h / 2)
                         bottom = math.ceil(cy + h / 2)
@@ -52,25 +53,27 @@ class CTPNPredict(Predict):
         return boxes
 
     def _group_boxes(self, boxes):
-        sorted_boxes = sorted(boxes, key=lambda x: x[0])   # sort according to left value
-        groups = [[sorted_boxes[0]]]
-        while len(sorted_boxes) > 0:
-            box = sorted_boxes[0]
+        boxes = sorted(boxes, key=lambda x: x[0])   # sort according to left value
+        groups = [[boxes[0]]]
+        while len(boxes) > 0:
+            box = boxes[0]
             left, top, right, bottom, *_ = box
             for group in groups:
-                box1 = group[-1]
-                left1, top1, right1, bottom1, _, o1 = box1
-                if left1 - right >= 50:
+                g_left, g_top, g_right, g_bottom, *_ = group[-1]
+
+                if left - g_right >= 50:
                     continue
-                if max(0, min(bottom, bottom1) - max(top, top1)) / \
-                        (max(bottom, bottom1) - min(top, top1)) <= 0.7:
+                if max(0, min(bottom, g_bottom) - max(top, g_top)) / \
+                        (max(bottom, g_bottom) - min(top, g_top)) <= 0.7:
                     continue
                 group.append(box)
                 break
             else:
                 groups.append([box])
 
-            sorted_boxes = sorted_boxes[1:]
+            boxes = boxes[1:]
+
+        groups = [group for group in groups if len(group) > GROUP_MIN_SAMPLES]
 
         return groups
 
